efffc1058446f7342c850cbb704aa2834fd7181e
yrakotoarinjatovo <yveslaurent.rakotoarinjatovo.prestataire@prima-solutions.com>
2020-04-06 23:34:31 +0200

    Init Angular app working with more module

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./front/front.module').then(m => m.FrontModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
